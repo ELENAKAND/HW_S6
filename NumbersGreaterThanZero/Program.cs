1dad85887a7320f9765d79494e9c443e62706515
ELENA <elenakand@icloud.com>
2022-07-15 13:46:01 +0300

    Количество чисел в массиве больше нуля

--- NumbersGreaterThanZero/Program.cs
+++ NumbersGreaterThanZero/Program.cs
@@ -6,7 +6,7 @@
 1, -7, 567, 89, 223-> 3
 */
  
- /*int GetNumber(string message)//Проверка числа, как и раньше
+int GetNumber(string message) //Проверка числа, как и раньше
 {
     int result = 0;
  
@@ -34,109 +34,50 @@
  
 return result;
 }
-            // проверка чисел массива:
-            int GetNumber1(string message1)
-            {
-                int result1 = 0;
- 
-                string errorMessage1 = ("Вы ввели не число. Введите корректное число.");
-                while (true)
-                {
-                    Console.WriteLine(message1);
- 
-                    if (int.TryParse(Console.ReadLine(), out result1))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine(errorMessage1);
-                    }
-                }
- 
-                return result1;
-            }
+
+int GetNumber1(string message1) // проверка чисел массива
+{
+int result1 = 0;
+ string errorMessage1 = ("Вы ввели не число. Введите корректное число.");
+    while (true)
+    {
+        Console.WriteLine(message1);
+        if (int.TryParse(Console.ReadLine(), out result1))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage1);
+        }
+    }
+ return result1;
+}
  
             //Ввод числа для проверки
-            int x = GetNumber("Введите  число элементов");
- 
-            int[] arr = new int[x];//массив чисел
+int x = GetNumber("Введите  число элементов");
+int[] arr = new int[x];//массив чисел
             //задаем числа:
-            for(int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = GetNumber1($"Введите {i}-й элемент");
-            }
-            
-            int count = 0;//счетчик чисел больше 0
-            for (int j = 0; j < arr.Length; j++)
-            {
-                Console.Write(arr[j] + "\t");
-                if (arr[j] > 0)
-                    count++;
-            }
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = GetNumber1($"Введите {i}-й элемент");
+    }
+int count = 0;//счетчик чисел больше 0
+    for (int j = 0; j < arr.Length; j++)
+    {
+        Console.Write(arr[j] + "\t");
+            if (arr[j] > 0)
+                count++;
+    }
  
-            Console.WriteLine($"Число элементов больше 0 = { count}");
-*/
+Console.WriteLine($"Число элементов больше 0 = { count}");
 
 
 
 
 
 
-
-
-                            //СЕМИНАР
-
-/*
-Задача 39: Напишите программу, которая перевернёт одномерный массив 
-(последний элемент будет на первом месте, а первый - на последнем и т.д.)
-[1 2 3 4 5] -> [5 4 3 2 1]
-[6 7 3 6] -> [6 3 7 6]
-*/
-
-int [] GetArray(int dimension)
-{
-    int [] arr = new int[dimension];
-    Random randomizer = new Random();
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = randomizer.Next(1,10);
-    }
-
-return arr;
-}
-void PrintArray(int [] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write($"{arr[i]} ");
-    }
-}
-
-int [] ArrayReverse(int [] arr)
-{
-    int [] resultArray = new int[arr.Length];
-    int count = 0;
-        for (int i = arr.Length - 1; i >= 0; i--)
-        {
-            resultArray[count] = arr[i];
-            count++;
-        }
-
-return resultArray;
-}
-Console.WriteLine("Начальный массив: ");
-
-int[] primaryArr = GetArray(6);
-PrintArray(primaryArr);
-Console.WriteLine();
-Console.WriteLine("Конечный массив: ");
-int[] resultArray = ArrayReverse(primaryArr);
-PrintArray(resultArray);
-Console.WriteLine();
-Console.WriteLine("Массив через Reverse(): ");
-PrintArray(primaryArr.Reverse().ToArray());
 
         
 
